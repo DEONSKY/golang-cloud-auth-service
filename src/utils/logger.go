15408da2b308beaf19f16c5cd1718e21d9c60b3c
Talha Emin Aydemir <talhaeminaydemir@gmail.com>
2023-01-28 10:14:54 +0300

    AUT-3 feat: logger debug level and prod level logs splitted

--- src/utils/logger.go
+++ src/utils/logger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -14,13 +15,41 @@ type Logger struct {
 	Module string
 }
 
+type ProdLevelLog struct {
+	App     string `json:"app"`
+	Module  string `json:"module"`
+	Message string `json:"message"`
+}
+
 type colors func(format string, a ...interface{}) string
 
-func (l *Logger) log(message string, color colors) {
+func (l *Logger) debugLevelLog(message string, color colors) {
 	date := time.Now()
 	format := color("[%s] | [%s] - [%s]")
 	cmd := fmt.Sprintf(format+": %s\n", date.Format(time.RFC3339Nano), l.App, l.Module, message)
 	io.WriteString(os.Stdout, cmd)
+}
+
+func (l *Logger) prodLevelLog(message string) {
+	logObject := &ProdLevelLog{
+		App:     l.App,
+		Module:  l.Module,
+		Message: message,
+	}
+	out, err := json.Marshal(logObject)
+	if err != nil {
+		fmt.Println(err, message)
+	}
+	fmt.Println(string(out))
+}
+
+func (l *Logger) log(message string, color colors) {
+	// FIXME: this control should check for env variable for if it's prod (maybe staging too) it will print logs as prodLevelLog
+	if true {
+		l.debugLevelLog(message, color)
+	} else {
+		l.prodLevelLog(message)
+	}
 }
 
 func (l *Logger) Info(message string) {
