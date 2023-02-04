41c502abca83d4ecbf7c260e4aaecb71102e9529
Talha Emin Aydemir <talhaeminaydemir@gmail.com>
2023-02-04 15:28:45 +0300

    ALE-10 fix: removed empt file, removed unused log

--- src/files/file.payloads.go
+++ src/files/file.payloads.go
@@ -1,13 +1,7 @@
 package files
 
-import "fmt"
-
 type FileUploadPayload struct {
 	FileName string
 	FileType string
 	FileSize int
-}
-
-func main() {
-	fmt.Println("vim-go")
 }
--- src/files/files.service.go
+++
@@ -1,7 +0,0 @@
-package files
-
-import "mime/multipart"
-
-func SaveFile(file *multipart.FileHeader, bucket string, key string) {
-
-}
